d837a3246cd9468b243e0855dffb429db7eb1515
jaiswalvinay7 <jaiswalvinay7@gmail.com>
2020-06-19 19:06:24 +0530

    Changes done in controller class

--- spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/HomeController.java
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/HomeController.java
@@ -34,7 +34,7 @@ public class HomeController {
 	public ResponseEntity<?>createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)throws Exception{
 		//Authenticate username password  
 		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),authenticationRequest.getPassword()));
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername                        (),authenticationRequest.getPassword()));
 		} catch (BadCredentialsException e) {
 			throw new Exception("Incorrect Username or Password",e);
 		}
